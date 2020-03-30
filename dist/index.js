dafaad9d2b4973aea9d1ac5a4ba0f02d13abef11
tirthamoulib <btirthamouli@gmail.com>
2020-03-30 10:36:37 +0530

    DOM interaction

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,18 @@
 "use strict";
-alert('Hello');
+var ProjectInput = (function () {
+    function ProjectInput(host, template) {
+        this.template = template;
+        this.host = host;
+    }
+    ProjectInput.prototype.render = function () {
+        var importedNode = document.importNode(this.template.content, true);
+        var element = importedNode.firstElementChild;
+        this.host.insertAdjacentElement('afterbegin', element);
+    };
+    return ProjectInput;
+}());
+(function () {
+    var projectInput = new ProjectInput(document.getElementById('app'), document.getElementById('project-input'));
+    projectInput.render();
+})();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,2 +1,48 @@
-// eslint-disable-next-line no-alert
-alert('Hello');
+class ProjectInput {
+  /**
+   * The main template for the form
+   */
+  private template: HTMLTemplateElement
+
+  /**
+   * Position where we are going to render
+   */
+  private host: HTMLDivElement
+
+  /**
+   * Constructor
+   * @param host
+   * @param template
+   */
+  constructor(host: HTMLDivElement, template: HTMLTemplateElement) {
+    this.template = template;
+    this.host = host;
+  }
+
+  /**
+   * Renders the elements in the page
+   */
+  render() {
+    // Step 1: Get the imported node
+    const importedNode = document.importNode(this.template.content, true);
+
+    // Step 2: Get the html
+    const element = importedNode.firstElementChild as HTMLFontElement;
+
+    // Step 3: Insert the node
+    this.host.insertAdjacentElement('afterbegin', element);
+  }
+}
+
+
+// Initializing
+(() => {
+  // Step 1: Create a new project instance
+  const projectInput = new ProjectInput(
+    document.getElementById('app') as HTMLDivElement,
+    document.getElementById('project-input') as HTMLTemplateElement,
+  );
+
+  // Step 2: Instanciate it
+  projectInput.render();
+})();
